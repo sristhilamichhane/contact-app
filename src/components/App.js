2644885cbdb26ebde7279a51babc59af24e3afe6
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2022-05-14 19:26:14 +0545

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
+
+  useEffect(() => {
+    async function init() {
+      const data = await localStorage.getItem(LOCAL_STORAGE_KEY);
+      setContacts(JSON.parse(data));
+    }
+    init();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContactList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ContactCard from "./ContactCard";
+
+const ContactList = (props) => {
+  const deleteConactHandler = (id) => {
+    props.getContactId(id);
+  };
+  const renderContactList = props.contacts.map((contact) => {
+    return (
+      <ContactCard
+        contact={contact}
+        clickHander={deleteConactHandler}
+        key={contact.id}
+      />
+    );
+  });
+  return <div className="ui celled list">{renderContactList}</div>;
+};
+
+export default ContactList;
